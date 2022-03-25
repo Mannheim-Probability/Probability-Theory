d62ffd455c81cb44528b94c61ba9bc95435ec05f
Github Action <noreply.actions@github.com>
2022-03-25 18:20:10 +0000

    645d5db8d15bbc6e962bf70eb49e3944bf4361ba

---
+++ exercises/js/collapsing_proofs.js
@@ -0,0 +1,23 @@
+function changeTag (node, tag) {
+	const clone = document.createElement(tag);
+	for (const attr of node.attributes) {
+	  clone.setAttributeNS(null, attr.name, attr.value);
+	}
+	while (node.firstChild) {
+	  clone.appendChild(node.firstChild);
+	}
+	node.replaceWith(clone);
+	return clone;
+}
+
+window.addEventListener("load", function() {
+	const proofs = document.getElementsByClassName("ltx_proof");
+
+	if(proofs.size != 0){
+		for(let proof of proofs) {
+			proof = changeTag(proof, "details");
+			changeTag(proof.children[0], "summary");
+		}
+	}
+
+})
